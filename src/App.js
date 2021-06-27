e6c551f587f840db5ffa94e29fd0ce9788bbc5df
Claire Reincastle <clairemr@hotmail.com>
2021-06-27 16:11:35 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>What's for dinner?</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          New Shopping List
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Add Recipe
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Add Ingredients
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
